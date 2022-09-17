87bd4435558a4d022974adba63c8c54d59108252
Gavilite <111202565+Gavilite@users.noreply.github.com>
2022-09-16 20:43:44 -0700

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,39 +1,19 @@
-#include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
-
- *print_numbers - function that print all numbers from
-
- *0 to 9
-
+ * print_numbers - prints 0123456789\n
  *
-
- *Return: returns nothing
-
+ * Return: void
  */
 
 
-
 void print_numbers(void)
-  
 {
-  
-  int n;
-  
+	char c;
 
-  
-  for (n = 48; n < 58; n++)
-    
-    {
-      
-      putchar(n);
-      
-    }
-  
-  putchar(10);
-  
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
